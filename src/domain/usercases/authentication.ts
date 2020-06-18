d6014fd54513d6dcf3123a5f8b72dbd1c83da1bb
wagner.silva <wagner.silva@softplan.com.br>
2020-06-18 06:27:21 -0300

    feat: ensure RemoteAuthentication calls HttpPostCliente with correct body

--- src/domain/usercases/authentication.ts
+++ src/domain/usercases/authentication.ts
@@ -1,6 +1,6 @@
-import { AccountModel } from 'domain/models/account-models'
+import { AccountModel } from '../../../domain/models/account-models'
 
-type AuthenticationParams = {
+export type AuthenticationParams = {
   email: string
   passowrd: string
 };
