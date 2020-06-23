a2b8f947ac4bf47d421d94912da7b73989f83f7a
wagner.silva <wagner.silva@softplan.com.br>
2020-06-23 06:55:30 -0300

    feat: ensure ValidationComposite return error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,20 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validatiors: FieldValidation[]) {}
+
+  validate (fieldName: string, fieldValue: string): string {
+    const validators = this.validatiors.filter(
+      validator => validator.field === fieldName
+    )
+
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+
+      if (error) {
+        return error.message
+      }
+    }
+  }
+}
