9a4a77be9bdd855e8d37868180e8a41380c18b9d
wagner.silva <wagner.silva@softplan.com.br>
2020-06-23 07:03:52 -0300

    refactor: move sut to a facotry method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,18 +1,29 @@
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationsSpies: FieldValidationSpy[]
+};
+
+const sutFactory = (): SutTypes => {
+  const fieldValidationsSpies = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field')
+  ]
+  const sut = new ValidationComposite(fieldValidationsSpies)
+
+  return {
+    sut,
+    fieldValidationsSpies
+  }
+}
+
 describe('ValidationComposite', () => {
   it('should return erro if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-
-    fieldValidationSpy.error = new Error('first_error_message')
-    fieldValidationSpy2.error = new Error('second_error_message')
-
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2
-    ])
+    const { sut, fieldValidationsSpies } = sutFactory()
+    fieldValidationsSpies[0].error = new Error('first_error_message')
+    fieldValidationsSpies[1].error = new Error('second_error_message')
 
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('first_error_message')
