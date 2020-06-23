5eb26e97dfb69f9a1ebe576db63484d4fb64fb76
wagner.silva <wagner.silva@softplan.com.br>
2020-06-23 06:05:39 -0300

    test: ensure emailValidation return falsy  if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,11 +1,19 @@
+import faker from 'faker'
 import { EmailValidation } from './email-validation'
 import { InvalidaFieldError } from '@/validation/erros'
 
 describe('EmailValidation', () => {
-  it('should  return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+  it('should return error if email is invalid', () => {
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate('')
 
     expect(error).toEqual(new InvalidaFieldError())
+  })
+
+  it('should return false if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+
+    expect(error).toBeFalsy()
   })
 })
