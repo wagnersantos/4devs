0ea3d67dc2e420a66d76b53e62e9fd09c4aa40e0
wagner.silva <wagner.silva@softplan.com.br>
2020-06-24 07:14:24 -0300

    refactor: add faker

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
+import faker from 'faker'
 import { render, RenderResult } from '@testing-library/react'
 
 import Input from './input'
 
 import Context from '@/presentation/contexts/form/form-context'
 
-const sutFactory = (): RenderResult => {
+const sutFactory = (fieldName: string): RenderResult => {
   return render(
     <Context.Provider value={{ state: {} }}>
-      <Input name="field" />
+      <Input name={fieldName} />
     </Context.Provider>
   )
 }
 
 describe('Input component', () => {
   it('should render', () => {
-    const sut = sutFactory()
-    const input = sut.getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    const sut = sutFactory(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
     expect(input).toBeTruthy()
   })
 })
