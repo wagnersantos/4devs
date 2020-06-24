ae33729c71d16ef01a3e4e8f5f02374c79a7fa92
wagner.silva <wagner.silva@softplan.com.br>
2020-06-24 07:10:41 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 
 import Input from './input'
 
 import Context from '@/presentation/contexts/form/form-context'
 
+const sutFactory = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name="field" />
+    </Context.Provider>
+  )
+}
+
 describe('Input component', () => {
   it('should render', () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name="field" />
-      </Context.Provider>
-    )
-
-    const input = getByTestId('field') as HTMLInputElement
+    const sut = sutFactory()
+    const input = sut.getByTestId('field') as HTMLInputElement
     expect(input).toBeTruthy()
   })
 })
