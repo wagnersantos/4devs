706ec36b00ccdcbe1ce745f0f28cc741f7445e69
wagner.silva <wagner.silva@softplan.com.br>
2020-06-26 06:38:09 -0300

    test: ensure localAccesToken throws if SetStorage throws

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -5,27 +5,37 @@ import { SetStorageMock } from '@/data/test/mock-storage'
 
 type SutTypes = {
   sut: LocalSaveAccesToken
-  setStorageSpy: SetStorageMock
+  setStorageMock: SetStorageMock
 };
 
 const sutFactory = (): SutTypes => {
-  const setStorageSpy = new SetStorageMock()
-  const sut = new LocalSaveAccesToken(setStorageSpy)
+  const setStorageMock = new SetStorageMock()
+  const sut = new LocalSaveAccesToken(setStorageMock)
 
   return {
     sut,
-    setStorageSpy
+    setStorageMock
   }
 }
 
 describe('LocalSaveAccesToken', () => {
   test('should call setStorage with correct value', async () => {
-    const { sut, setStorageSpy } = sutFactory()
+    const { sut, setStorageMock } = sutFactory()
     const accessToken = faker.random.uuid()
 
     await sut.save(accessToken)
 
-    expect(setStorageSpy.key).toBe('accessToken')
-    expect(setStorageSpy.value).toBe(accessToken)
+    expect(setStorageMock.key).toBe('accessToken')
+    expect(setStorageMock.value).toBe(accessToken)
+  })
+
+  test('should throw if SetStorageMock throws', async () => {
+    const { sut, setStorageMock } = sutFactory()
+
+    jest.spyOn(setStorageMock, 'set').mockRejectedValue(new Error())
+
+    const promise = sut.save(faker.random.uuid())
+
+    await expect(promise).rejects.toThrow(new Error())
   })
 })
