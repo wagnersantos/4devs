bd4e3b128dd15a6124c1545bfaa9288602b46d3f
wagnersantos <wagtvc@gmail.com>
2020-06-29 12:01:42 -0300

    test: ensure SignUp calls addAccount with correct values

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -9,10 +9,11 @@ import {
 } from '@testing-library/react'
 
 import { SignUp } from '@/presentation/pages'
-import { Helper, ValidationStub } from '@/presentation/test'
+import { Helper, ValidationStub, AddAccountSpy } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
+  addAccountSpy: AddAccountSpy
 };
 
 type SutParams = {
@@ -21,12 +22,17 @@ type SutParams = {
 
 const sutFactory = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
+  const addAccountSpy = new AddAccountSpy()
+
   validationStub.errorMessage = params?.validationError
 
-  const sut = render(<SignUp validation={validationStub} />)
+  const sut = render(
+    <SignUp validation={validationStub} addAccount={addAccountSpy} />
+  )
 
   return {
-    sut
+    sut,
+    addAccountSpy
   }
 }
 
@@ -138,5 +144,21 @@ describe('Signup', () => {
 
     await simulateValidSubmit(sut)
     Helper.testElementExists(sut, 'spinner')
+  })
+
+  it('should call addAccount with correct value', async () => {
+    const { sut, addAccountSpy } = sutFactory()
+    const name = faker.name.findName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+
+    await simulateValidSubmit(sut, name, email, password)
+
+    expect(addAccountSpy.params).toEqual({
+      name,
+      email,
+      password,
+      passwordConfirmation: password
+    })
   })
 })
---
+++ src/presentation/test/mock-add-account.ts
@@ -0,0 +1,14 @@
+import { AddAccountParams, AddAccount } from '@/domain/usecases'
+import { mockAccountModel } from '@/domain/test'
+import { AccountModel } from '@/domain/models'
+
+export class AddAccountSpy implements AddAccount {
+  account = mockAccountModel();
+  params: AddAccountParams;
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    this.params = params
+
+    return this.account
+  }
+}
