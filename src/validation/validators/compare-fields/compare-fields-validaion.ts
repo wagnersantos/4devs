700744dd04a043b4c21398f95be7cc68ec51a695
wagnersantos <wagtvc@gmail.com>
2020-06-29 06:38:14 -0300

    feat: ensure compareFieldsalidation returns falsy if validation succeeds

--- src/validation/validators/compare-fields/compare-fields-validaion.ts
+++ src/validation/validators/compare-fields/compare-fields-validaion.ts
@@ -8,6 +8,8 @@ export class CompareFieldsValidation implements FieldValidation {
   ) {}
 
   validate (value: string): Error {
-    return new InvalidFieldError()
+    const isDiff = value !== this.valuteToCompare
+
+    return isDiff ? new InvalidFieldError() : null
   }
 }
