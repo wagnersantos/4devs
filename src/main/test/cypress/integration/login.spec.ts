91523266a10210307e597106ce4f8ae5008800d2
wagnersantos <wagtvc@gmail.com>
2020-06-30 13:50:33 -0300

    test: ensure login present valid state if form valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -23,12 +23,27 @@ describe('Login', () => {
       .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
 
-    cy.getByTestId('password').type(faker.random.word())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3))
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Valor inválido')
       .should('contain.text', '🔴')
 
     cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present valid state if form is valid', () => {
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'tudo certo')
+      .should('contain.text', '🉑')
+
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'tudo certo')
+      .should('contain.text', '🉑')
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
