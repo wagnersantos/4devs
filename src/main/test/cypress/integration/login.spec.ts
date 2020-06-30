00e6816612a1f9b1d70dfe367fcf83bf5cc233e8
wagnersantos <wagtvc@gmail.com>
2020-06-30 13:42:25 -0300

    test: ensure login present error state if form invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -10,6 +12,22 @@ describe('Login', () => {
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo Obrigatório')
       .should('contain.text', '🔴')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+
+    cy.getByTestId('password').type(faker.random.word())
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴')
+
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
