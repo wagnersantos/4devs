9bb43da13aa4c4f0dfd3b2e0dbdfac311370993d
wagnersantos <wagtvc@gmail.com>
2020-07-02 05:55:14 -0300

    test: prevent login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -138,4 +138,21 @@ describe('Login', () => {
       assert.isOk(window.localStorage.getItem('accessToken'))
     )
   })
+
+  it('should prevent multiple submits', () => {
+    cy.route({
+      method: 'POST',
+      url: /login/,
+      status: 200,
+      response: {
+        accessToken: faker.random.uuid()
+      }
+    }).as('request')
+
+    cy.getByTestId('email').type(faker.internet.email())
+    cy.getByTestId('password').type(faker.random.alphaNumeric(5))
+
+    cy.getByTestId('submit').dblclick()
+    cy.get('@request.all').should('have.length', 1)
+  })
 })
