00bb0cca6a3f2a788c057d15a64c0b08d1dd30a0
wagnersantos <wagtvc@gmail.com>
2020-07-04 05:53:41 -0300

    feat: ensure serveyList renders 4 empty lis on start

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Header, Footer } from '@/presentation/components'
 
+import { SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
+
 import styles from './survey-list-styles.scss'
 
 const SurveyList: React.FC = () => {
@@ -11,7 +13,8 @@ const SurveyList: React.FC = () => {
 
       <div className={styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul>
+        <ul data-testid="survey-list">
+          <SurveyItemEmpty />
         </ul>
       </div >
       <Footer />
