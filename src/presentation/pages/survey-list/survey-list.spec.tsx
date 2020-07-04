e8800643e55c24652946fccad3810ebb83440b37
wagnersantos <wagtvc@gmail.com>
2020-07-04 06:12:23 -0300

    test: ensure SurveyList calls loadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -4,14 +4,39 @@ import { render } from '@testing-library/react'
 import { Helper } from '@/presentation/test'
 
 import SurveyList from './survey-list'
+import { LoadSurveyList } from '@/domain/usecases'
+import { SurveyModel } from '@/domain/models'
 
-const sutFactory = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0
+
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const sutFactory = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList', () => {
   it('should present 4 empty items on start', () => {
     sutFactory()
     Helper.testChildCount('survey-list', 4)
+  })
+
+  it('should call loadSurveyList', () => {
+    const { loadSurveyListSpy } = sutFactory()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
