85f690b83f5887ee3278695c2ebe1439cfca6157
wagnersantos <wagtvc@gmail.com>
2020-07-04 18:29:17 -0300

    feat: ensure surveyList logout on accessDeniedError

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,21 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { Header, Footer } from '@/presentation/components'
 
 import { SurveyContext, SurveyListItem, Error } from '@/presentation/pages/survey-list/components'
 import { LoadSurveyList } from '@/domain/usecases'
 
+import { AccessDeniedError } from '@/domain/errors'
+import { ApiContext } from '@/presentation/contexts'
+
 import styles from './survey-list-styles.scss'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
+
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = useState({
     surveys: [] as LoadSurveyList.Model[],
     error: '',
     reload: false
   })
+  const { setCurrentAccount } = useContext(ApiContext)
+  const history = useHistory()
+
   const { error, reload } = state
 
   useEffect(() => {
@@ -24,6 +32,12 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
         const surveys = await loadSurveyList.loadAll()
         setState({ ...state, surveys })
       } catch (error) {
+        if (error instanceof AccessDeniedError) {
+          setCurrentAccount(undefined)
+          history.replace('/login')
+          return
+        }
+
         setState({ ...state, error: error.message })
       }
     })()
