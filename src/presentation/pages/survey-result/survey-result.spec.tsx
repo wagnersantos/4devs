22fae6fbd25388f9019b43f8ca91b7d96be6f949
wagnersantos <wagtvc@gmail.com>
2020-07-07 15:33:21 -0300

    test: ensure surveyResult present correct result on success

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 
-import { mockAccountModel, LoadSurveyResultSpy } from '@/domain/test'
+import { mockAccountModel, LoadSurveyResultSpy, mockSurveyResultModel } from '@/domain/test'
 import { ApiContext } from '@/presentation/contexts'
 import { Helper } from '@/presentation/test'
 import { SurveyResult } from '@/presentation/pages'
 
 type SutTypes = {
-  loadSurveyListSpy: LoadSurveyResultSpy
+  loadSurveyResultSpy: LoadSurveyResultSpy
 }
 
-const sutFactory = (): SutTypes => {
-  const loadSurveyListSpy = new LoadSurveyResultSpy()
+const sutFactory = (surveyResult = mockSurveyResultModel()): SutTypes => {
+  const loadSurveyResultSpy = new LoadSurveyResultSpy()
+  loadSurveyResultSpy.surveyResult = surveyResult
 
   render(
     <ApiContext.Provider value={{
       setCurrentAccount: jest.fn(),
       getCurrentAccount: () => mockAccountModel()
     }}>
-      <SurveyResult loadSurveyResult={loadSurveyListSpy} />)
+      <SurveyResult loadSurveyResult={loadSurveyResultSpy} />)
 
     </ApiContext.Provider>
   )
   return {
-    loadSurveyListSpy
+    loadSurveyResultSpy
   }
 }
 describe('SurveyResult', () => {
@@ -38,8 +39,39 @@ describe('SurveyResult', () => {
   })
 
   it('should call loadSurveyResult', async () => {
-    const { loadSurveyListSpy } = sutFactory()
+    const { loadSurveyResultSpy } = sutFactory()
     await waitFor(() => screen.getByTestId('survey-result'))
-    expect(loadSurveyListSpy.callsCount).toBe(1)
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
+  })
+
+  it('should present surveyResult data on success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2020-01-10T00:00:00')
+    })
+    sutFactory(surveyResult)
+    await waitFor(() => screen.getByTestId('survey-result'))
+
+    expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
+    Helper.testChildCount('answers', 2)
+
+    const answerWrap = screen.queryAllByTestId('answer-wrap')
+    expect(answerWrap[0]).toHaveClass('active')
+    expect(answerWrap[1]).not.toHaveClass('active')
+
+    const images = screen.queryAllByTestId('image')
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+    expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
   })
 })
