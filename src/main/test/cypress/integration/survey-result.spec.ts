028053aa77ff39ef29c41dde117c64e62b5e40bc
wagnersantos <wagtvc@gmail.com>
2020-07-08 05:33:25 -0300

    test: ensure surveyResult reloads on reload button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -22,4 +22,27 @@ describe('SurveyResult', () => {
     cy.visit('/surveys/any_id')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
   })
+
+  it('should reload on button click', () => {
+    cy.route({
+      method: 'GET',
+      url: /surveys/,
+      status: faker.helpers.randomize([400, 404, 500]),
+      response: {
+        error: faker.random.words()
+      }
+    }).as('request')
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+
+    cy.route({
+      method: 'GET',
+      url: /surveys/,
+      status: 200,
+      response: 'fx:survey-result'
+    }).as('request')
+
+    cy.getByTestId('reload').click()
+    cy.getByTestId('question').should('exist')
+  })
 })
